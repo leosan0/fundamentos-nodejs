bb5b96f4e7d252703ade3a99df5757e2c17fbef7
Leonardo Okoti <leowokoti@gmail.com>
2020-08-26 13:08:48 -0300

    Desafio completo.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeBalance = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(income => income.value)
+      .reduce((total, income) => (total += income), 0);
+
+    const outcomeBalance = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(outcome => outcome.value)
+      .reduce((total, outcome) => (total += outcome), 0);
+
+    const totalBalance = incomeBalance - outcomeBalance;
+
+    const balance = { incomeBalance, outcomeBalance, totalBalance };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Invalid transaction. Balance can not be negative');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
